f5fce857d879c99fe03fbd512fc7e1ef124f1004
Ryan S <ryans@dev2dev.com>
2020-07-14 14:50:48 -0400

    Refactored to use data attribute to store slideshow state

--- app/javascript/controllers/slideshow_controller.js
+++ app/javascript/controllers/slideshow_controller.js
@@ -3,21 +3,30 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "slide" ]
   initialize() {
-    this.showSlide(0)
+    const index = parseInt(this.element.getAttribute('data-slideshow-index'))
+    this.showCurrentSlide()
   }
 
   next() {
-    this.showSlide(this.index + 1)
+    this.index++
   }
 
   previous() {
-    this.showSlide(this.index - 1)
+    this.index--
   }
 
-  showSlide(index) {
-    this.index = index
+  showCurrentSlide() {
     this.slideTargets.forEach((el, i) => {
-      el.classList.toggle("slide--current", index == i)
+      el.classList.toggle("slide--current", this.index == i)
     })
+  }
+
+  get index() {
+    return parseInt(this.data.get('index'))
+  }
+
+  set index(value) {
+    this.data.set('index', value)
+    this.showCurrentSlide()
   }
 }
