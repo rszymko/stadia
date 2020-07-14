06ac20b71380585e910d03cfd936acaacd57e519
Ryan S <ryans@dev2dev.com>
2020-07-14 13:52:38 -0400

    Added initial image gallery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,10 @@ import '../stylesheets/application'
 //
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
