81680f5278ba51f62fa038791b154d34a789474e
Ryan S <ryans@dev2dev.com>
2020-07-16 18:11:41 -0400

    Added Slideshow Previous and Next feature scenarios

--- spec/features/slideshow_spec.rb
+++ spec/features/slideshow_spec.rb
@@ -7,4 +7,12 @@ RSpec.feature 'Slideshow', type: :feature do
     expect(page).to have_selector('[data-action="slideshow#next"]', count: 1)
     expect(page).to have_selector('.slide--current', count: 1)
   end
+  scenario 'next and previous buttons', js: true do
+    visit root_path
+    expect(find('.slide--current').text).to eq 'Boston Garden'
+    click_on('Next')
+    expect(find('.slide--current').text).to eq 'Camden Yards'
+    click_on('Previous')
+    expect(find('.slide--current').text).to eq 'Boston Garden'
+  end
 end
